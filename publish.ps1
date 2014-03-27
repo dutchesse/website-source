f364117b16c193719444b2abb83151abd7113d16
Don Smith <locksmithdon@gmail.com>
2014-03-27 20:33:51 +1300

    Add new blog entries

--- publish.ps1
+++ publish.ps1
@@ -2,15 +2,16 @@ cd "C:\Users\donsm_000\repos\website-source"
 echo "Building website"
 grunt
 
+echo "Commiting changes to website source"
+git add .
+git commit -m "Add new blog entries"
+echo "Pushing source changes to GitHub"
+git push origin master
+
 echo "Commiting new website"
+cd dutchesse.github.io
 git add .
 git commit -m "Add new blog entries"
 echo "Pushing changes to GitHub"
 git push origin master
 
-echo "Commiting changes to website source"
-cd ..
-git add .
-git commit -m "Add new blog entries"
-echo "Pushing source changes to GitHub"
-git push origin master
